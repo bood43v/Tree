f9c828d4f3950bfdaf4b2eca1d7799bb7cfef308
bood43v <budaevgena@gmail.com>
2023-09-13 13:14:01 +0900

    Добавьте файлы проекта.

---
+++ Tree/Tree/Tree.cpp
@@ -0,0 +1,49 @@
+﻿/// Работа с классом TreeNode - узел бинарного дерева
+/// @author Будаев Г.Б.
+#include <iostream>
+#include "TreeNode.h"
+
+using namespace std;
+
+int main()
+{
+    // Создание узлов дерева с данными
+    TreeNode<int>* A = new TreeNode<int>(11);
+    TreeNode<int>* B = new TreeNode<int>(22);
+    TreeNode<int>* C = new TreeNode<int>(33);
+    TreeNode<int>* D = new TreeNode<int>(44);
+    TreeNode<int>* E = new TreeNode<int>(55);
+
+
+    // Установка левого и правого потомка для узла A
+    A->SetLeft(B);
+    A->SetRight(C);
+
+    B->SetLeft(D);
+    B->SetRight(E);
+
+    //// Получение данных из узла
+    //cout << "Data of A: " << A->GetData() << endl;
+    //// Функцией
+    //A->PrintNodeData();
+    
+    A->PrintTreeNLR();
+    cout << endl;
+    A->PrintTreeLNR();
+    cout << endl;
+    A->PrintTreeLRN();
+
+    /// Удаление узлов
+    A->DeleteTree();
+
+    A->PrintNodeData();
+    B->PrintNodeData();
+    C->PrintNodeData();
+    D->PrintNodeData();
+    E->PrintNodeData();
+
+
+
+
+    return 0;
+}
---
+++ Tree/Tree/TreeNode.h
@@ -0,0 +1,126 @@
+/// Класс TreeNode - узел бинарного дерева
+/// @author Будаев Г.Б.
+#pragma once
+using namespace std;
+
+template<class T>
+class TreeNode
+{
+private:
+    T data;                 /// Данные
+    TreeNode<T>* left;      /// Левый потомок
+    TreeNode<T>* right;     /// Правый потомок
+
+public:
+    /// Конструктор без параметров
+    TreeNode(T data_){
+        data = data_; 
+        left = nullptr;
+        right = nullptr;
+    }
+
+    /// Конструктор без параметров
+    TreeNode() {
+        data = NULL;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    /// Деструктор
+    ~TreeNode(){}
+
+    /// Установить левого потомка
+    void SetLeft(TreeNode<T>* node_)
+    {
+        left = node_;
+    }
+
+    /// Установить правого потомка
+    void SetRight(TreeNode<T>* node_)
+    {
+        right = node_;
+    }
+
+    /// Вернуть данные
+    T GetData() const
+    {
+        return data;
+    }
+    /// Вернуть левого потомка
+    TreeNode<T>* GetLeft() const
+    {
+        return left;
+    }
+
+    /// Вернуть правого потомка
+    TreeNode<T>* GetRight() const
+    {
+        return right;
+    }
+
+    /// Удалить дерево
+    void DeleteTree()
+    {
+        TreeNode<T>* node_;
+        node_ = this;
+        if (node_ == nullptr)
+            return;
+
+        node_->GetLeft()->DeleteTree();
+        node_->GetRight()->DeleteTree();
+        delete node_;
+    }
+
+    /// Вывести данные узла
+    void PrintNodeData()
+    {
+        if (this != nullptr)
+        {
+            cout << "Data of node: " << this->GetData() << endl;
+        }
+        else
+        {
+            cout << "Node is null" << endl;
+        }
+    }
+
+    /// Вывести дерево NLR
+    void PrintTreeNLR()
+    {
+        TreeNode<T>* node_;
+        node_ = this;
+        if (node_ == nullptr)
+            return;
+        node_->PrintNodeData();
+        node_->GetLeft()->PrintTreeNLR();
+        node_->GetRight()->PrintTreeNLR();
+    }
+
+    /// Вывести дерево LNR 
+    void PrintTreeLNR()
+    {
+        TreeNode<T>* node_;
+        node_ = this;
+        if (node_ == nullptr)
+            return;
+        node_->GetLeft()->PrintTreeLNR();
+        node_->PrintNodeData();
+        node_->GetRight()->PrintTreeLNR();
+    }
+
+    /// Вывести дерево LRN
+    void PrintTreeLRN()
+    {
+        TreeNode<T>* node_;
+        node_ = this;
+        if (node_ == nullptr)
+            return;
+        node_->GetLeft()->PrintTreeLRN();
+        node_->GetRight()->PrintTreeLRN();
+        node_->PrintNodeData();
+    }
+
+};
+
+
+
